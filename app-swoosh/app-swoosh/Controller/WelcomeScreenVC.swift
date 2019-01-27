1c2e0871ec945a25c5a990bdc4f3a955415dc2cf
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-01-26 22:57:46 -0700

    Lesson 06 and 07

--- app-swoosh/app-swoosh/Controller/WelcomeScreenVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeScreenVC.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class WelcomeScreenVC: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
     
-    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
