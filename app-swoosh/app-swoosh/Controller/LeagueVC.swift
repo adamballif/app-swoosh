8f4bc927ba3b1dc2438c38d83b84c1ae17eb4caf
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-01-27 21:19:46 -0700

    Struct for Buttons

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextButton: BorderButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
@@ -20,7 +26,24 @@ class LeagueVC: UIViewController {
     @IBAction func onNextTap(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-   // @IBAction func onNextTapped(_ sender: Any) {
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
+    // @IBAction func onNextTapped(_ sender: Any) {
         //performSegue(withIdentifier: "skillVCSegue", sender: self)
     //}
     
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Adam Ballif on 1/27/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
